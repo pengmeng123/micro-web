353ab7868eba12843faa2855f8ca21c5b21c6f95
pengmeng <pengmeng@qcc.com>
2025-06-19 14:18:37 +0800

    ff

--- react-child/src/App.js
+++ react-child/src/App.js
@@ -3,14 +3,6 @@ import MicroImage from "./components/micro-image";
 import "./App.css";
 
 function App() {
-  console.log("xx----", window.__MICRO_APP_ENVIRONMENT__);
-  // 使用完整的URL路径或相对路径
-  const logoSrc = window.__MICRO_APP_ENVIRONMENT__
-    ? window.__MICRO_APP_PUBLIC_PATH__ + logo
-    : logo;
-
-  console.log("");
-
   return (
     <div className="App">
       <MicroImage src={logo} />
