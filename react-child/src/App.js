872290c56cbb52b4db5c19a57f7286a5cec387dd
pengmeng <pengmeng@qcc.com>
2025-06-19 11:21:15 +0800

    2

--- react-child/src/App.js
+++ react-child/src/App.js
@@ -1,7 +1,13 @@
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">react-child应用</div>;
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className="title">react-child应用</p>
+    </div>
+  );
 }
 
 export default App;
--- vue2-child/src/App.jsx
+++ vue2-child/src/App.jsx
@@ -5,6 +5,7 @@ export default {
         sdfdf22333s
         <router-view />
         <micro-app name="vue3-child" url="http://localhost:8081/"></micro-app>
+        <micro-app name="react-child" url="http://localhost:3001/"></micro-app>
       </div>
     );
   },
