67b8ff4427181d74f5f9c6694b031b44cb8289f8
pengmeng <pengmeng@qcc.com>
2025-07-19 15:44:34 +0800

    ff

--- react-child/src/setupProxy.js
+++ react-child/src/setupProxy.js
@@ -13,21 +13,9 @@ module.exports = function (app) {
 
       onProxyReq: function (proxyReq, req, res) {
         console.log("📤 Proxying:", req.method, req.url);
-
-        // 确保 cookie 被正确传递
-        if (req.headers.cookie) {
-          proxyReq.setHeader("Cookie", req.headers.cookie);
-          console.log("🍪 Cookie found:", req.headers.cookie);
-        }
       },
       onProxyRes: function (proxyRes, req, res) {
-        console.log("📥 Response:", proxyRes.statusCode, req.url);
-
-        // 设置 CORS 头
-        const origin = req.headers.origin;
-        if (origin) {
-          proxyRes.headers["Access-Control-Allow-Origin"] = origin;
-        }
+        proxyRes.headers["Access-Control-Allow-Origin"] = "*";
         proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
       },
       onError: function (err, req, res) {
